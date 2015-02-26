25d968770e93314ef5bb5a4e5f1c6161c9640d76
Jesse Carter <jctr073@gmail.com>
2015-02-25 18:24:29 -0800

    Wed. commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Blog::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-
   resources :articles
 
   # You can have the root of your site routed with "root"
