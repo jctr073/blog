e4e99af9ba0cf13780321daf7139bb9bea2a5cdd
Jesse Carter <jctr073@gmail.com>
2015-02-26 18:34:03 -0800

    adding form processing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,23 @@
 class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render text: params[:article].inspect
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
   end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+
 end
